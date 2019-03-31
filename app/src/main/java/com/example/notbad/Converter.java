7918c1177789a8f04f1866264d045a568b4ebfc6
longlonglu <qq836482561@gmail.com>
2019-03-31 14:26:57 -0400

    123

--- app/src/main/java/com/example/notbad/Converter.java
+++ app/src/main/java/com/example/notbad/Converter.java
@@ -12,11 +12,18 @@ import android.widget.Button;
 public class Converter extends Fragment {
 
     Button distance;
+    Button energy;
+    Button force;
+    Button sound;
+    Button speed;
+    Button temperature;
+    Button time;
+    Button weight;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.converter_frgment, container, false);
 
-        distance = (Button) view.findViewById(R.id.CDistance);
+        distance = (Button) view.findViewById(R.id.CLength);
         distance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,7 +32,68 @@ public class Converter extends Fragment {
             }
         });
 
+        energy = (Button) view.findViewById(R.id.CEnergy);
+        energy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Energy.class);
+                startActivity(intent);
+            }
+        });
 
+        force = (Button) view.findViewById(R.id.CForce);
+        force.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Force.class);
+                startActivity(intent);
+            }
+        });
+
+        sound = (Button) view.findViewById(R.id.CSound);
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Sound.class);
+                startActivity(intent);
+            }
+        });
+
+        speed = (Button) view.findViewById(R.id.CSpeed);
+        speed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Speed.class);
+                startActivity(intent);
+            }
+        });
+
+        temperature = (Button) view.findViewById(R.id.CTemperature);
+        temperature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Temperature.class);
+                startActivity(intent);
+            }
+        });
+
+        time = (Button) view.findViewById(R.id.CTime);
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Time.class);
+                startActivity(intent);
+            }
+        });
+
+        weight = (Button) view.findViewById(R.id.CWeight);
+        weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Weight.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/example/notbad/Time.java
@@ -0,0 +1,98 @@
+package com.example.notbad;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Time extends AppCompatActivity {
+    HashMap<Integer, Double> length = new HashMap<>();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time);
+
+        final Spinner spinner_from = (Spinner) findViewById(R.id.spinner_from);
+        final Spinner spinner_to = (Spinner) findViewById(R.id.spinner_to);
+        final Button convert = (Button) findViewById(R.id.convert);
+        final EditText from = (EditText) findViewById(R.id.value_from);
+        final TextView to = (TextView) findViewById(R.id.value_to);
+        initializeHashMap();
+
+        List<String> units = new ArrayList<String>();
+        units.add("Meter");
+        units.add("Kilometer");
+        units.add("Centimeter");
+        units.add("Millimeter");
+        units.add("Inch");
+        units.add("Foot");
+        units.add("Mile");
+        units.add("Yard");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, units);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_from.setAdapter(dataAdapter);
+        spinner_to.setAdapter(dataAdapter);
+
+        spinner_from.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+        spinner_to.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                to.setText(convert(length, from.getText().toString(),spinner_from.getSelectedItemPosition(), spinner_to.getSelectedItemPosition()));
+            }
+        });
+    }
+
+    private void initializeHashMap() {
+        length.put(0, 1.0);
+        length.put(1, 1000.0);
+        length.put(2, 0.01);
+        length.put(3, 0.001);
+        length.put(4, 0.0254);
+        length.put(5, 0.3048);
+        length.put(6, 1609.34);
+        length.put(7, 0.9144);
+    }
+
+    private String convert(HashMap<Integer, Double> converter, String value, int from, int to) {
+        try {
+            double d = Double.parseDouble(value);
+            d = d * converter.get(from) / converter.get(to);
+            return String.format("%.10f", d);
+        } catch (Exception e) {
+            return "N/A";
+        }
+    }
+
+}
